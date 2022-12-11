9cd3414c4a8c6520c560e531a5221332bd81d175
Daniel <mrpickles123115@gmail.com>
2022-12-11 11:15:08 -0700

    changed to sample PWM signal

--- Logic_Analyser.c
+++ Logic_Analyser.c
@@ -6,11 +6,11 @@
 #include "hardware/dma.h"
 #include "hardware/pio.h"
 #include "data_analyser.pio.h"
+#include "hardware/pwm.h"
 
-#define captureSize 432
-#define capturePin 13
-#define vS 14
-#define decode 0
+#define captureSize 450
+#define capturePin 11
+#define signal 12
 
 void dmaHandler();
 static inline void setupDma(PIO, uint);
@@ -20,15 +20,17 @@ uint32_t buffer[captureSize];
 uint dmaChan;
 PIO pio;
 uint sm;
+const bool invertBools = false;
 const bool triggerLevel = true;
-const bool captureOnce = true;
+const bool captureOnce = false;
 
 int main()
 {
     stdio_init_all();
     dmaChan = dma_claim_unused_channel(true);
     // float clkdiv = 125000000 / (38000 * 5);
-    float clkdiv = 50;
+
+    float clkdiv = 2500;
 
     irq_set_exclusive_handler(DMA_IRQ_0, dmaHandler);
     irq_set_enabled(DMA_IRQ_0, true);
@@ -47,15 +49,27 @@ int main()
     // gpio_set_dir(vS, true);
     // gpio_put(vS, true);
     // for interfacing with sensor
+
     // gpio_pull_up(capturePin);
+    gpio_pull_down(capturePin);
+
 
     setupDma(pio, sm);
     setupPIO(pio, sm, offset, capturePin, triggerLevel, clkdiv);
+
+    gpio_set_function(signal, GPIO_FUNC_PWM);
+    pwm_config c = pwm_get_default_config();
+    pwm_config_set_wrap(&c, 49999);
+    pwm_config_set_clkdiv(&c, 50);
+    pwm_init(pwm_gpio_to_slice_num(signal), &c, true);
+    pwm_set_gpio_level(signal, 3250);
+
 
     while (true)
     {
         // printf("%u\n", pio_sm_get_blocking(pio, sm));
         tight_loop_contents();
+
         // printf("%d\n", dma_channel_is_busy(dmaChan));
     }
     return 0;
@@ -63,79 +77,48 @@ int main()
 
 void dmaHandler()
 {
-    // printf("Received, proccessing ready\n");
-    bool state = true;
+    printf("Received, proccessing ready\n");
+    uint8_t start = buffer[0] & 1u;
+    bool state = (bool) ((buffer[0] & 1u) == 1); //lsb starts getting proccessed first
+    state = (invertBools) ? !state: state;
     bool currentState;
     uint32_t stateWidth = 0;
-    uint32_t tempWidth;
-    uint32_t extractedData = 0;
     for (int i = 0; i < captureSize; i++)
     {
         for (int j = 0; j < 32; j++) //iterate from lsb of data
         {
-            currentState = !((bool) ((buffer[i] >> j) && 1u)); //1 is off 0 is on
+            currentState = ((bool) ((buffer[i] >> j) & 1u));
+            if (invertBools)
+            {
+                currentState = !currentState;
+            }
             if (currentState == state)
             {
                 stateWidth += 1;
             }
             if (currentState != state)
             {
-                if (!decode)
-                {
-                    printf("State: %s, Width: %d\n", state ? "True": "False", stateWidth);
-                }
-                if (decode)
-                {
-                    if (state)
-                    {
-                        tempWidth = stateWidth; // the on time divided by off time will determine if the signal is on or off
-                    }
-                    if (!state) //it is false so divide true by false
-                    {
-                        float temp = (float) tempWidth / stateWidth;
-                        // printf("%2.3f\n", temp);
-                        if (temp < 1.2)
-                        {
-                            extractedData *= 2;
-                            if (temp < 0.5)
-                            {
-                                extractedData += 1;
-                            }
-                        }
-
-                    }
-                }
+                printf("State: %s, Width: %d\n", state ? "True": "False", stateWidth);
 
                 stateWidth = 1;
                 state = !state;
             }
         }
     }
-    if ((stateWidth != 1) && (!decode))
+    if (stateWidth != 1)
     {
         printf("State: %s, Width: %d\n", state ? "True": "False", stateWidth);
     }
-    printf("Transfer done\n");
-
-    if (decode)
-    {
-        char n2 = extractedData & (char) 255;
-        char n1 = (extractedData >> 8) & (char) 255;
-        if (n1 + n2 == 255)
-        {
-            // printf("%u\n", extractedData);
-            printf("Raw: %u, Command: %u, Repeat: %d\n", extractedData, n1 >> 1, n1 & 1u);
-        }
-    }
+    // printf("Transfer done\n");
 
 
     dma_channel_acknowledge_irq0(dmaChan);
     pio_sm_exec(pio, sm, pio_encode_wait_pin(triggerLevel, 0));
+    pio_sm_clear_fifos(pio, sm);
     if (!captureOnce) //don't restart
     {
-        dma_channel_set_write_addr(dmaChan, &buffer, true);
+        dma_channel_set_write_addr(dmaChan, &buffer, triggerLevel);
     }
-    pio_sm_clear_fifos(pio, sm);
     return;
 }
 static inline void setupPIO(PIO pio, uint sm, uint offset, uint pin, bool trigger, float clkdiv)
